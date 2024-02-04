4bfa725c9f54df33a8bc27adb0a3f2fa9d464b41
caesar003 <caesarmuksid@gmail.com>
2024-02-04 23:51:20 +0700

    nothing

---
+++ plugins/community.lua
@@ -0,0 +1,36 @@
+return {
+  "AstroNvim/astrocommunity",
+  -- { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim", enabled = true },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.github-nvim-theme"}, 
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      transparent_background = true,
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        rainbow_delimiters = true,
+      },
+    },
+  },
+  -- { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
+}
